e3abd2f7f40fedf2e954e03a0bafad1f3b271c04
Anderson <anderson.tam05@gmail.com>
2023-11-05 18:03:49 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 // ref: https://dev.to/cloudx/how-to-use-puppeteer-inside-a-docker-container-568c
 import puppeteer from "puppeteer";
 
+//use headless mode with config headless: 'new' for new headless implementation
 const browser = await puppeteer.launch({
     executablePath: '/usr/bin/google-chrome',
-    // headless: false,
+    headless: false,
     // args:[
     //     '--no-sandbox',
     //     '--disable-setuid-sandbox'
     // ]
+    
 });
-let page = await browser.newPage();
-await page.goto("https://google.com")
-await page.screenshot(
-    {
-        path: "test.png"
-    }
-)
-await browser.close();
+let page = await browser.pages()[0].page;
+await page.goto("https://youtube.com", {
+    waitUntil: 'networkidle2'
+})
+// await page.screenshot(
+//     {
+//         path: "test.png"
+//     }
+// );
+// browser.waitUntil()
+// await browser.close();
